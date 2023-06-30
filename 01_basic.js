24c90d2f369d0e3f0da65f7a588a14706d447dcd
sohyuno <osh_1004@naver.com>
2023-06-30 22:01:22 +0900

    변수

--- 01_basic.js
+++ 01_basic.js
@@ -4,4 +4,14 @@ console.log(10);
 console.log(10, 20 ,30);
 
 
-console.log('오소현 멋있어') 
+console.log('오소현 멋있어 나이는', 20 + 6 , '입니다');
+
+//제어문자, 문자열 str 안에서 사용이 되며 특수한 문자를 나타낼 때 사용함
+//문자열에서 특수문자의 표현 
+//엔터값 (\n), Tab (\t)
+
+console.log('안녕 \n야호');
+console.log('안녕하세요\t반갑네요')
+
+// \를 표현하고싶다?
+console.log('제 폴더는 c:\\osh 입니다')
---
+++ 01_variable.js
@@ -0,0 +1,6 @@
+console.log('02파일출력')
+
+qqq = 10;
+lll = '끝내주세요..~';
+
+console.log(lll)
