e264c8418e7f2eac1f3aeaf472b4c54c667028c2
sohyuno <osh_1004@naver.com>
2023-07-03 21:58:34 +0900

    day02 end

--- day02.js
+++ day02.js
@@ -15,8 +15,39 @@ console.log(variable1)
 //선언 후 최초로 대입한 값이 없다면 undefined가 출력됨
 console.log('=============================')
 let variable2;
-variable2 = '오소현';
+variable2 = '오소현'; //값을 최초로 대입하는 행위 -> 초기화
 console.log(variable2);
 //재선언 불가능
 
+
+//변수를 선언하면서 동시에 초기화를 해줘야 한다
+const variable3 = '오소현최고야';
+console.log(variable3);
+// variable3 = 58;
+// console.log(variable3); -> 재할당 불가능
+
+console.log('==========연산자===========');
+console.log('덧셈 연산자~~~~~~~!');
+console.log('10 + 20 :', 10 + 20);
+//str + str 
+console.log('10'+'안녕');
+// str + undefined
+console.log(undefined + '얏홍');
+// str + number -> str
+console.log(10 + '안녕');
+console.log(50 + '50');
+//NaN -> num타입 
+console.log(undefined + 80);
+//여러가지 해보기
+console.log(20 + 41 + '20' + 70 + 45);
+console.log(10+''+27);
+
+console.log('뺼셈 연산자~~~~~~~~~~~~~~~!');
+
+console.log('안녕'-'안녕'); //NaN
+console.log('10' - 2); //number 타입 결과 8
+console.log(undefined - 1);
+
+console.log('나눗셈 해볼까욘..~~~~~!?');
+console.log(10 / 0); // Infinity
 
