7b2a61e8e2db9d4eb0363179fa35f2dfed71f19f
sohyuno <osh_1004@naver.com>
2023-07-05 20:22:25 +0900

    백틱씀

--- day03.js
+++ day03.js
@@ -5,8 +5,10 @@ console.log(false ? 10 : 100);
 
 console.log('---------prompt--------------')
 console.log(prompt('너의 이름은?','스즈메'));
+
 //사용자에게 아이디를 입력 받는다
 //반갑습니다 ***님 
 
 let id = prompt('아이디를 입력해주세요');
-console.log( '반값습니다' , id , '님의 포인트는 1,200점 입니다');
+console.log( `반값습니다. ${id}님의 포인트는 1,200점 입니다`);
+console.log(typeof id);
