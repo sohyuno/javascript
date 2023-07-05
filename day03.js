41fadf849d751e938033f9302e7b7a764f0d23ed
sohyuno <osh_1004@naver.com>
2023-07-05 21:04:34 +0900

    객체

--- day03.js
+++ day03.js
@@ -1,7 +1,7 @@
-console.log('잘 연결됨')
+// console.log('잘 연결됨')
 
-console.log(true ? 10 : 100);
-console.log(false ? 10 : 100);
+// console.log(true ? 10 : 100);
+// console.log(false ? 10 : 100);
 
 // console.log('---------prompt--------------')
 // console.log(prompt('너의 이름은?','스즈메'));
@@ -21,10 +21,22 @@ console.log(false ? 10 : 100);
 // document.write(`두 수를 더한 결과는 ${Number(num1) + Number(num2)}입니다`);
 // document.write(`<h1>두 수를 더한 결과는 ${num1 + 50}입니다`);
 
-//경고 메세지를 띄어주는 함수
-console.log('_________alert____________');
-alert('잘못된 접근입니다만?');
+// //경고 메세지를 띄어주는 함수
+// console.log('_________alert____________');
+// alert('잘못된 접근입니다만?');
 
-//확인 메세지를 띄어주는 함수
-console.log('__________confirm_______');
-confirm('그래도 정말 나가셔야겠습니까?');
+// //확인 메세지를 띄어주는 함수
+// //확인을 클릭하면 true 취소는 false
+// console.log('__________confirm_______');
+// let check = confirm('그래도 정말 나가셔야겠습니까?');
+// document.write(check);
+
+console.log('___________객체___________')
+//객체는 {}를 사용하여 만든다
+let book1 = {
+    title : '대모험',
+    page:205,
+    author:'오소현',
+    publisher:'오또출판사',
+    price:35500}
+console.log(book1);        
