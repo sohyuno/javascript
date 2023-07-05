52914eee94df89eddef2a596fb94b7fa348b384d
sohyuno <osh_1004@naver.com>
2023-07-05 20:50:24 +0900

    alert

--- day03.js
+++ day03.js
@@ -15,8 +15,16 @@ console.log(false ? 10 : 100);
 
 
 //prompt 함수 실행결과는 str 혹은 null
-let num1 = Number(prompt('숫자1입력>>'));
-let num2 = Number(prompt('숫자2입력 >>'));
+// let num1 = Number(prompt('숫자1입력>>'));
+// let num2 = Number(prompt('숫자2입력 >>'));
 
-document.write(`두 수를 더한 결과는 ${Number(num1) + Number(num2)}입니다`);
-document.write(`두 수를 더한 결과는 ${num1 + 50}입니다`);
+// document.write(`두 수를 더한 결과는 ${Number(num1) + Number(num2)}입니다`);
+// document.write(`<h1>두 수를 더한 결과는 ${num1 + 50}입니다`);
+
+//경고 메세지를 띄어주는 함수
+console.log('_________alert____________');
+alert('잘못된 접근입니다만?');
+
+//확인 메세지를 띄어주는 함수
+console.log('__________confirm_______');
+confirm('그래도 정말 나가셔야겠습니까?');
